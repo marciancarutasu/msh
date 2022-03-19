be0eb3d2e4a09ebebabec15c19d610b5b56f9600
marciancarutasu <marciancarutasu@gmail.com>
2022-03-19 11:22:30 +0200

    products page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { Home, Products, Contact } from './pages';
 import { Nav } from './layout';
 import { Footer } from './layout';
+import {
+  CSSTransition,
+  TransitionGroup,
+} from 'react-transition-group';
 
 import {
   BrowserRouter as Router,
@@ -11,15 +15,22 @@ import {
 
 function App() {
   return (
-    <Router>
-      <Nav />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <TransitionGroup>
+      <CSSTransition
+        timeout={300}
+        classNames="fade">
+        <Router>
+          <Nav />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </CSSTransition>
+    </TransitionGroup>
+
   );
 };
 
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,7 +1,40 @@
 import React from 'react';
 import { Layout } from '../layout';
+import Product from "../components/productCard";
 
 const Products = () =>
-  <Layout>This is the products page.</Layout>;
+  <Layout>
+  <div className="productsFilters">
+    <a href="#" className="filterBtn men grey active">Men</a>
+    <a href="#" className="filterBtn men grey">Women</a>
+    <a href="#" className="filterBtn filters grey">Filters</a>
+  </div>
+    <div className="row productsScreen">
+      <div className="col">
+        <Product></Product>
+      </div>
+      <div className="col">
+        <Product></Product>
+      </div>
+      <div className="col">
+        <Product></Product>
+      </div>
+      <div className="col">
+        <Product></Product>
+      </div>
+      <div className="col">
+        <Product></Product>
+      </div>
+      <div className="col">
+        <Product></Product>
+      </div>
+      <div className="col">
+        <Product></Product>
+      </div>
+      <div className="col">
+        <Product></Product>
+      </div>
+    </div>
+  </Layout>;
 
 export default Products;
